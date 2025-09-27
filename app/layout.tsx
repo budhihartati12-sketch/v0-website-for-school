d5cd804d45b3170227c7cea0b361be8849e1951e
dev <dev@server1>
2025-09-27 10:09:39 +0700

    perbaikan font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,16 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Geist, Manrope } from "next/font/google"
-import "./globals.css"
 import { MobileBottomNav } from "@/components/site/mobile-nav"
+import { Manrope } from "next/font/google"
+import "./globals.css"
 
-const geist = Geist({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-geist",
-})
 
 const manrope = Manrope({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-manrope",
 })
+
 
 export const metadata: Metadata = {
   title: "SMP IT Masjid Syuhada Yogyakarta",
@@ -28,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="id" className={`${geist.variable} ${manrope.variable} antialiased`}>
+    <html lang="id" className={`${manrope.variable} ${manrope.variable} antialiased`}>
       <body className="font-sans">
         <div className="min-h-dvh pb-16 md:pb-0">{children}</div>
         <MobileBottomNav />
